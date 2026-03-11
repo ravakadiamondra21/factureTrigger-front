37ce55f1059dd1acb55d5a8478dec2d26773c272
ravakadiamondra21 <ravakadiamondra00@gmail.com>
2026-03-12 00:34:57 +0300

    counting audit per action

--- src/app/core/services/auditService/audit.service.ts
+++ src/app/core/services/auditService/audit.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CountingAuditModel } from 'src/app/models/countingAuditModel';
 import { environment } from 'src/environments/environment.development';
 import { UserService } from '../userService/user.service';
 
@@ -23,5 +24,17 @@ export class AuditService {
     });
 
     return this.httpClient.get(`${this.apiUrl}/getAll`, { headers });
+  }
+
+  public countingAudit(): Observable<CountingAuditModel[]> {
+    const token = this.userService.getToken();
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.httpClient.get<CountingAuditModel[]>(`${this.apiUrl}/count`, {
+      headers,
+    });
   }
 }
